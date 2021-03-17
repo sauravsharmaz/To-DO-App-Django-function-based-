5c37831702fb48774dac187a7e544718e7359703
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-03-18 01:09:16 +0530

    fetched data from database to show on home page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns= [
 	path('',views.index,name='main'),
+	path('desc/',views.desc,name='result'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,27 @@ def index(request):
 	tasks= Task.objects.all()
 	tasks={'all_tasks':tasks}
 	return render(request, 'Home/index.html',tasks)		
+
+def desc(request):
+	tasks= Task.objects.all()
+	tasks={'all_tasks':tasks}
+	return render(request,'Home/result.html',tasks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
