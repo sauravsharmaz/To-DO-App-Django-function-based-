71944a4cd2a234019155fa6b28ce50036a473659
sauravsharmaz <saurav.sharma.zz45@gmail.com>
2021-03-18 23:27:54 +0530

    added bootstrap to the main page and modified some models

--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 comp_choices= (('NO','no'),('YES','yes'))
 
 class Task(models.Model):
-	title= models.CharField(max_length=200)
+	title= models.CharField(max_length=200,default='No title')
 	description= models.CharField(max_length=1000,default='No desc. provided')
 	is_completed= models.CharField(max_length=3, choices=comp_choices,default= 'NO')
+	time= models.DateTimeField(default=timezone.now)
 
 	def __str__(self):
 		return self.title	
--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from myapp.models import Task
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 def index(request):
 	tasks= Task.objects.all()
@@ -10,10 +10,15 @@ def index(request):
 	return render(request, 'Home/index.html',tasks)		
 
 def desc(request):
-	tasks= Task.objects.all()
-	tasks={'all_tasks':tasks}
-	return render(request,'Home/result.html',tasks)
-
+	# create a new todo item
+	titl= request.POST['title']
+	descr= request.POST['description']
+	comp= request.POST['is_completed']
+	new_item= Task(title=titl,description=descr,is_completed=comp)
+	# save
+	new_item.save()
+	# redirect the browser to '/'
+	return HttpResponseRedirect('/')
 
 
 
